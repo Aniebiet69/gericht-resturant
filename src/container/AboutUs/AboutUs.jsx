9a54c9b2caafad6bd773d0b09a4c321977e7e642
Aniebiet Archibong <102147581+Surebo-i@users.noreply.github.com>
2023-03-08 09:27:29 +0100

    about

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {images} from '../../constants'
 
 import './AboutUs.css';
 
@@ -7,6 +8,28 @@ const AboutUs = () => (
     <div className="app__aboutus-overlay flex__center">
       <img src= {images.G} alt="g letter"/>
     </div>
+
+    <div className='app__aboutus-content flex__center '>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+          <img src= {images.spoon} alt="aboutus_spoon" className='spoon__img' />
+          <p className='p__opensans'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+          <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='about_knife' />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+          <img src= {images.spoon} alt="aboutus_spoon" className='spoon__img' />
+          <p className='p__opensans'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+          <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+    </div>
+
   </div>
 );
 
